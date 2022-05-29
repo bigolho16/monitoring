e07bc2b3eff050fdf9bdcbd5e70585195115dc29
Leandro <43631792+bigolho16@users.noreply.github.com>
2022-05-28 20:58:53 -0400

    Delete arquivo_php.jpg.php

--- arquivo_php.jpg.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Use ls command to shell_exec
-// function
-$output = shell_exec('echo Terminal funcionando');
-  
-// Display the list of all file
-// and directory
-echo "<pre>$output</pre>";
-?>
